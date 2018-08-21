7f03f2fe87f2e3b92b3c58543bfb21a818972f2a
21892 <1311639728@qq.com>
2018-08-21 21:17:04 +0800

    idea git test

--- test-git/src/com/hxjc/test/GitTest.java
+++ test-git/src/com/hxjc/test/GitTest.java
@@ -3,7 +3,9 @@ package com.hxjc.test;
 public class GitTest {
 
 	public static void main(String[] args) {
-		System.out.println("GitTest.main()");
+
+		 	System.out.println("GitTest.main()");
+		System.out.println("idea.main()");
 
 	}
 
